6b83762c05d80a0c2c701bdbef8095b2e496bde6
cnmrrt <57072756+cnmrrt@users.noreply.github.com>
2024-02-20 22:26:07 +0300

    .

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,5 +1,10 @@
 export default function Navbar() {
     return (
-        <ul id="navbar"><li><strong><a href="/">Home</a></strong></li></ul>
+        <ul id="navbar">
+            <li><strong><a href="/">Home</a></strong></li>
+            <li><strong><a href="/authors">Authors</a></strong></li>
+            <li><strong><a href="/words">Words</a></strong></li>
+            <li><strong><a href="/preverbs">Preverbs</a></strong></li>
+        </ul>
     )
 }
