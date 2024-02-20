f1fee28de7eee41f3e2bbaf0cdf62f8660d8087c
cnmrrt <57072756+cnmrrt@users.noreply.github.com>
2024-02-21 00:19:35 +0300

    .

--- app/preverbs/page.tsx
+++ app/preverbs/page.tsx
@@ -1,4 +1,46 @@
+import Link from 'next/link';
+import Script from 'next/script'
+import Navbar from '../components/navbar';
+import type { Metadata } from 'next';
+import { GoogleTagManager } from '@next/third-parties/google';
 
-export default function Preverbs() {
-    return <h1>Hello, Home page!</h1>
+async function getPreverbs() { const res = await fetch('https://words-from-life-5cb26-default-rtdb.firebaseio.com/idioms%20and%20proverbs/en/preverbs.json'); if (!res.ok) { throw new Error('Failed to fetch data') } return res.json() }
+
+export const metadata: Metadata = {
+  title: 'Preverbs',
+  description: '',
+  alternates: {
+    canonical: "https://wordsfromlife.com/preverbs",
   }
+}
+
+export default async function Preverbs() {
+  const preverbsData = await getPreverbs();
+  return (
+    <html lang="en">
+      <head>
+        <Script src="/static/js/macy.js" />
+      </head>
+      <body>
+        <GoogleTagManager gtmId="GTM-TKDV62Q" />
+        <Navbar />
+        <main className="main" role="main">
+          <h1>Preverbs</h1>
+          <div className="home-container">
+            <section className="section">
+              <ul>
+                {
+                  preverbsData.map((item: any) =>
+                  (
+                    <li className="" key={item.id}><Link href={"/preverbs/" + item.id}>{item.preverb}</Link></li>
+                  )
+                  )
+                }
+              </ul>
+            </section>
+          </div>
+        </main>
+      </body>
+    </html>
+  )
+}
--- app/words/page.tsx
+++ app/words/page.tsx
@@ -1,4 +1,46 @@
+import Link from 'next/link';
+import Script from 'next/script'
+import Navbar from '../components/navbar';
+import type { Metadata } from 'next';
+import { GoogleTagManager } from '@next/third-parties/google';
 
-export default function Words() {
-    return <h1>Hello, Home page!</h1>
+async function getWords() { const res = await fetch('https://words-from-life-5cb26-default-rtdb.firebaseio.com/words.json'); if (!res.ok) { throw new Error('Failed to fetch data') } return res.json() }
+
+export const metadata: Metadata = {
+  title: 'Words',
+  description: '',
+  alternates: {
+    canonical: "https://wordsfromlife.com/words",
   }
+}
+
+export default async function Words() {
+  const wordsData = await getWords();
+  return (
+    <html lang="en">
+      <head>
+        <Script src="/static/js/macy.js" />
+      </head>
+      <body>
+        <GoogleTagManager gtmId="GTM-TKDV62Q" />
+        <Navbar />
+        <main className="main" role="main">
+          <h1>Words</h1>
+          <div className="home-container">
+            <section className="section">
+              <ul>
+                {
+                  wordsData.map((item: any) =>
+                  (
+                    <li className="" key={item.id}><Link href={"/words/" + item.id}>{item.word}</Link></li>
+                  )
+                  )
+                }
+              </ul>
+            </section>
+          </div>
+        </main>
+      </body>
+    </html>
+  )
+}
