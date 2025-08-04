9e96c43d1f6b4b6d3bdf3014da85986ffd88bdbf
cnmrrt <57072756+cnmrrt@users.noreply.github.com>
2025-08-04 04:13:53 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,14 +5,7 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
-   <html lang="en">
-      <Head>
-        <meta name="google-adsense-account" content="ca-pub-5581015881313741" />
-      </Head>
-      <body>{children}</body>
-    </html>
-  );
+}>) {<>{children}</>);
 }
 
 
