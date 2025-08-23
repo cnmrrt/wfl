13fe792d7769f08ca29717e084d233743ffff5fd
cnmrrt <57072756+cnmrrt@users.noreply.github.com>
2025-08-23 16:28:24 +0300

    Update page.tsx

--- app/words/[slug]/page.tsx
+++ app/words/[slug]/page.tsx
@@ -90,14 +90,15 @@ export default async function country({ params }: { params: { slug: string } }) 
               <div id="related-words">
                 <span id="related-words-title">Related Words</span>
                 <ul id="related-words-list">
-                  {item.related_words.map((subItem: any, index: number) => (
-                    <li key={index}>
-                      <a
-                        href={`https://wordsfromlife.com/words/${subItem.toLowerCase()}`}
-                        dangerouslySetInnerHTML={{ __html: subItem }}
-                      />
-                    </li>
-                  ))}
+                  {Array.isArray(item?.related_words) &&
+                    item.related_words.map((subItem: any, index: number) => (
+                      <li key={index}>
+                        <a
+                          href={`https://wordsfromlife.com/words/${subItem.toLowerCase()}`}
+                          dangerouslySetInnerHTML={{ __html: subItem }}
+                        />
+                      </li>
+                    ))}
                 </ul>
               </div> 
             </main>
