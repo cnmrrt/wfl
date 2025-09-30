54047a997d24b1c81039597579401467a9214aa7
cnmrrt <mertcann.1034@gmail.com>
2025-09-30 16:59:55 +0300

    .

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1759240026872,
+    __BUILD_TIMESTAMP_MS__: 1759240473976,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
