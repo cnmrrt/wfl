06b2438ce030eaec4041c0b025d391c5e49a306f
cnmrrt <mertcann.1034@gmail.com>
2025-10-02 04:38:36 +0300

    Update page.tsx

--- app/words/page.tsx
+++ app/words/page.tsx
@@ -6,6 +6,7 @@ import { GoogleTagManager } from '@next/third-parties/google';
 
 async function getWords() { const res = await fetch('https://words-from-life-5cb26-default-rtdb.firebaseio.com/words.json'); if (!res.ok) { throw new Error('Failed to fetch data') } return res.json() }
 
+
 export const metadata: Metadata = {
   title: 'List of Words',
   description: '',
