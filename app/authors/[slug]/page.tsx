310eba32c923fc3dd3904a29b64d8da54a8a7567
cnmrrt <mertcann.1034@gmail.com>
2025-10-02 17:21:47 +0300

    Update page.tsx

--- app/authors/[slug]/page.tsx
+++ app/authors/[slug]/page.tsx
@@ -1,3 +1,11 @@
+export async function generateStaticParams() {
+    const res = await fetch('https://words-from-life-5cb26-default-rtdb.firebaseio.com/quotes/authors-new.json');
+    if (!res.ok) throw new Error('Failed to fetch data');
+    const data = await res.json();
+    return data
+        .filter((item: any) => item.en_quotes !== "" && item.img != null)
+        .map((item: any) => ({ slug: (item.id).toLowerCase() + "-quotes" }));
+}
 import Link from 'next/link';
 import type { Metadata, ResolvingMetadata } from 'next'
 import { GoogleTagManager } from '@next/third-parties/google';
